1b9ea5b014d678ff6825ec6fb01ca431c0239822
Dan Bagnell <daniel.p.bagnell@gmail.com>
2016-01-25 15:09:44 -0500

    Lines from the center of the star burst WIP.

--- Apps/Declutter/DeclutterDemo.js
+++ Apps/Declutter/DeclutterDemo.js
@@ -9,24 +9,44 @@ require({
     'Cesium/Core/BoundingRectangle',
     'Cesium/Core/Cartesian2',
     'Cesium/Core/Cartesian3',
+    'Cesium/Core/Cartesian4',
+    'Cesium/Core/Color',
+    'Cesium/Core/ColorGeometryInstanceAttribute',
     'Cesium/Core/defined',
+    'Cesium/Core/GeometryInstance',
+    'Cesium/Core/Math',
+    'Cesium/Core/Matrix4',
     'Cesium/Core/ScreenSpaceEventHandler',
     'Cesium/Core/ScreenSpaceEventType',
+    'Cesium/Core/SimplePolylineGeometry',
     'Cesium/DataSources/KmlDataSource',
     'Cesium/Scene/Billboard',
+    'Cesium/Scene/PerInstanceColorAppearance',
+    'Cesium/Scene/Primitive',
     'Cesium/Scene/SceneTransforms',
+    'Cesium/ThirdParty/when',
     'Cesium/Widgets/Viewer/Viewer',
     'domReady!'
 ], function(
     BoundingRectangle,
     Cartesian2,
     Cartesian3,
+    Cartesian4,
+    Color,
+    ColorGeometryInstanceAttribute,
     defined,
+    GeometryInstance,
+    CesiumMath,
+    Matrix4,
     ScreenSpaceEventHandler,
     ScreenSpaceEventType,
+    SimplePolylineGeometry,
     KmlDataSource,
     Billboard,
+    PerInstanceColorAppearance,
+    Primitive,
     SceneTransforms,
+    when,
     Viewer) {
     "use strict";
 
@@ -35,12 +55,42 @@ require({
     var viewer = new Viewer('cesiumContainer', {
         selectionIndicator : false
     });
-    viewer.dataSources.add(KmlDataSource.load('./KML/mcs.kmz'));
+
+    var kmlPromise = KmlDataSource.load('./KML/mcs.kmz');
+    viewer.dataSources.add(kmlPromise);
+
+    when(kmlPromise).then(function(dataSource) {
+        var values = dataSource.entities.values;
+        var length = values.length;
+
+        for (var i = 0; i < length; ++i) {
+            var entity = values[i];
+            if (defined(entity.billboard) && defined(entity.label)) {
+                entity.label.show = false;
+            }
+        }
+    });
 
     var scene = viewer.scene;
 
+    function wgs84ToWindowCoordinates(position) {
+        var actualPosition = SceneTransforms.computeActualWgs84Position(scene.frameState, position);
+        var camera = scene.camera;
+        var viewProjection = Matrix4.multiply(camera.frustum.projectionMatrix, camera.viewMatrix, new Matrix4());
+        var positionCC = new Cartesian4();
+        Matrix4.multiplyByVector(viewProjection, Cartesian4.fromElements(actualPosition.x, actualPosition.y, actualPosition.z, 1.0, positionCC), positionCC);
+
+        var result = new Cartesian3();
+        SceneTransforms.clipToGLWindowCoordinates(scene, positionCC, result);
+        result.y = scene.canvas.clientHeight - result.y;
+        result.z = positionCC.z / positionCC.w;
+        return result;
+    }
+
     var pickedEntities;
-    var rectangle;
+    var lines;
+    var linePrimitive;
+    var radius;
     var centerPosition = new Cartesian3();
     var padding = 10.0;
 
@@ -50,22 +100,13 @@ require({
             var pickedObjects = scene.drillPick(movement.position);
             if (defined(pickedObjects) && pickedObjects.length > 1) {
                 pickedEntities = [];
+                lines = [];
 
-                var x = 0.0;
-                var y = 0.0;
-                var width = 0.0;
-                var height = 0.0;
-
-                var dirX = 1.0;
-                var dirY = 0.0;
-
-                var maxX = 0.0;
-                var minX = 0.0;
-                var maxY = 0.0;
-                var minY = 0.0;
-
-                var maxWidth = Number.NEGATIVE_INFINITY;
-                var maxHeight = Number.NEGATIVE_INFINITY;
+                var angle = 0.0;
+                var angleIncrease;
+                var magnitude;
+                var magIncrease;
+                var maxDimension;
 
                 for (var i = 0; i < pickedObjects.length; ++i) {
                     var object = pickedObjects[i];
@@ -76,68 +117,96 @@ require({
 
                         pickedEntities.push(object);
 
-                        var offset = new Cartesian2(x, y);
+                        if (!defined(angleIncrease)) {
+                            var width = object.primitive.width;
+                            var height = object.primitive.height;
+                            maxDimension = Math.max(width, height) + padding;
+                            magnitude = maxDimension + maxDimension * 0.5;
+                            magIncrease = magnitude;
+                            angleIncrease = maxDimension / magnitude;
+                        }
+
+                        var x = magnitude * Math.cos(angle);
+                        var y = magnitude * Math.sin(angle);
+
+                        var offset = new Cartesian2(x, -y);
                         object.id.billboard.pixelOffset = offset;
                         if (defined(object.id.label)) {
                             object.id.label.pixelOffset = offset;
-                            object.id.label.show = false;
                         }
 
-                        var objectWidth = object.primitive.width + padding;
-                        var objectHeight = object.primitive.height + padding;
-                        x += objectWidth * dirX;
-                        y += objectHeight * dirY;
+                        var position = wgs84ToWindowCoordinates(object.primitive.position);
+                        position.x += offset.x;
+                        position.y += offset.y;
+                        var worldPosition = SceneTransforms.drawingBufferToWgs84Coordinates(scene, position, position.z);
+                        lines.push(worldPosition);
 
-                        maxX = Math.max(maxX, x);
-                        minX = Math.min(minX, x);
-                        maxY = Math.max(maxY, y);
-                        minY = Math.min(minY, y);
-
-                        maxWidth = Math.max(objectWidth, maxWidth);
-                        maxHeight = Math.max(objectHeight, maxHeight);
-
-                        var w = maxX - minX;
-                        var h = maxY - minY;
-
-                        if (w > width) {
-                            width = w
-
-                            dirY = dirX > 0.0 ? 1.0 : -1.0;
-                            dirX = 0.0;
-                        }
-                        if (h > height) {
-                            height = h;
-
-                            dirX = dirY > 0.0 ? -1.0 : 1.0;
-                            dirY = 0.0;
+                        angle += angleIncrease;
+                        if (angle + angleIncrease * 0.5 > CesiumMath.TWO_PI) {
+                            magnitude += magIncrease;
+                            angle = 0.0;
+                            angleIncrease = maxDimension / magnitude;
                         }
                     }
                 }
 
-                if (width > 0.0 || height > 0.0) {
-                    rectangle = new BoundingRectangle();
-                    rectangle.width = width + maxWidth;
-                    rectangle.height = height + maxHeight;
-
-                    viewer.selectedEntity = undefined;
-                } else {
-                    pickedEntities = undefined;
+                var instances = [];
+                for (var j = 0; j < lines.length; ++j) {
+                    instances.push(new GeometryInstance({
+                        geometry : new SimplePolylineGeometry({
+                            positions : [centerPosition, lines[j]],
+                            followSurface : false,
+                            granularity : CesiumMath.PI_OVER_FOUR
+                        }),
+                        attributes : {
+                            color : ColorGeometryInstanceAttribute.fromColor(Color.WHITE)
+                        }
+                    }));
                 }
+
+                linePrimitive = scene.primitives.add(new Primitive({
+                    geometryInstances : instances,
+                    appearance : new PerInstanceColorAppearance({
+                        flat : true,
+                        translucent : false
+                    }),
+                    asynchronous : true
+                }));
+
+                lines = undefined;
+                viewer.selectedEntity = undefined;
+                radius = magnitude + magIncrease;
             }
         }
     }, ScreenSpaceEventType.LEFT_CLICK);
 
+    scene.camera.moveStart.addEventListener(function() {
+        if (!defined(radius)) {
+            return;
+        }
+        
+        for (var i = 0; i < pickedEntities.length; ++i) {
+            var entity = pickedEntities[i].id;
+            entity.billboard.pixelOffset = new Cartesian2(0.0, 0.0);
+            if (defined(entity.label)) {
+                entity.label.pixelOffset = new Cartesian2(0.0, 0.0);
+                entity.label.show = false;
+            }
+        }
+        scene.primitives.remove(linePrimitive);
+        linePrimitive = undefined;
+        pickedEntities = undefined;
+        radius = undefined;
+    });
+
     var currentObject;
 
     handler.setInputAction(function(movement) {
-        if (defined(rectangle)) {
+        if (defined(radius)) {
             var screenPosition = SceneTransforms.wgs84ToWindowCoordinates(scene, centerPosition);
-            screenPosition.x -= rectangle.width * 0.5;
-            screenPosition.y -= rectangle.height * 0.5;
 
             var position = movement.endPosition;
-            if (position.x < screenPosition.x || position.x > screenPosition.x + rectangle.width ||
-                position.y < screenPosition.y || position.y > screenPosition.y + rectangle.height) {
+            if (Cartesian2.distance(position, screenPosition) > radius) {
                 for (var i = 0; i < pickedEntities.length; ++i) {
                     var entity = pickedEntities[i].id;
                     entity.billboard.pixelOffset = new Cartesian2(0.0, 0.0);
@@ -146,8 +215,10 @@ require({
                         entity.label.show = false;
                     }
                 }
+                scene.primitives.remove(linePrimitive);
+                linePrimitive = undefined;
                 pickedEntities = undefined;
-                rectangle = undefined;
+                radius = undefined;
             } else {
                 var pickedObject = scene.pick(movement.endPosition);
                 if (pickedObject !== currentObject) {
